c00d22e31a43e50dca23802d43816a223ce63087
ahmedezzat85 <ahmedezzateasa@gmail.com>
2017-12-25 15:56:20 +0200

    Change train.sh

--- python/train.sh
+++ python/train.sh
@@ -1,12 +1,19 @@
 PYTHON=python3
 BE=tensorflow
 DATASET=CIFAR-10
-NUM_EPOCH=200
+NUM_EPOCH=100
 BATCH_SZ=100
-L2_REG=0.00001
 FP16=0
-FMT=NHWC
-model=snpx_net
+FMT=NCHW
+model=resnet
+LR=0.1
+DATA_AUG=0
+MODEL_LOG_DIR=resnet-sgd-wd
+OPT=sgd
+L2_REG=0
+EPOCH=0
 
-$PYTHON snpx_train_classifier.py --backend $BE --model-name $model --target-data $DATASET --data-format $FMT \
-	--num-epoch $NUM_EPOCH --batch-size $BATCH_SZ --use-fp16 $FP16
+$PYTHON snpx_train_classifier.py --backend $BE --model-name $model --target-data $DATASET \
+	--data-format $FMT --num-epoch $NUM_EPOCH --batch-size $BATCH_SZ --use-fp16 $FP16     \
+	--optimizer $OPT --lr $LR --l2-reg $L2_REG --logs-subdir $MODEL_LOG_DIR --data-aug $DATA_AUG \
+	--begin-epoch $EPOCH
