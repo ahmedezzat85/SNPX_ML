6dcc2eb5ce7690e8e38d967d6bd6179479398c35
ahmedezzat85 <ahmedezzateasa@gmail.com>
2018-01-11 14:51:18 +0200

    correct google drive downloadable links

--- python/cifar10_download.sh
+++ python/cifar10_download.sh
@@ -1,5 +1,5 @@
 mkdir snpx/datasets
 mkdir snpx/datasets/CIFAR-10
-wget https://drive.google.com/open?id=13XDDKM4t60ermEsDEW3r_lwQ40Ya7T_V -O snpx/datasets/CIFAR-10/CIFAR-10.mean
-wget https://drive.google.com/open?id=1_MvQ44m3kh3H_uq_RxI1yOii9wsbQ3NQ -O snpx/datasets/CIFAR-10/CIFAR-10-val.tfrecords
-wget https://drive.google.com/open?id=1gD3_yTIJ7onknBRD998QYA8ciIGBCpFA -O snpx/datasets/CIFAR-10/CIFAR-10-train.tfrecords
+wget "https://drive.google.com/uc?export=download&id=13XDDKM4t60ermEsDEW3r_lwQ40Ya7T_V" -O snpx/datasets/CIFAR-10/CIFAR-10.mean
+wget "https://drive.google.com/uc?export=download&id=1_MvQ44m3kh3H_uq_RxI1yOii9wsbQ3NQ" -O snpx/datasets/CIFAR-10/CIFAR-10-val.tfrecords
+wget "https://drive.google.com/uc?export=download&id=1gD3_yTIJ7onknBRD998QYA8ciIGBCpFA" -O snpx/datasets/CIFAR-10/CIFAR-10-train.tfrecords
