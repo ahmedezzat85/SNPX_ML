e3e84988c3a25f67c5c4db341168dae8f4f960ba
ahmedezzat85 <ahmedezzateasa@gmail.com>
2018-01-11 17:20:48 +0200

    ok

--- python/init.sh
+++ python/init.sh
@@ -2,9 +2,9 @@ cd
 git clone https://github.com/ahmedezzat85/SNPX_ML.git
 sudo chmod 777 SNPX_ML -R
 cd SNPX_ML/python
+source activate tensorflow_p36
 
 # Download the dataset
 ./cifar10_download.sh
 
-source activate tensorflow_p36
 ./train.sh
